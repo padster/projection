317398894cb979e13d75769efb6195898406f3c3
padster <padsterpat@gmail.com>
2015-05-25 12:07:31 +1200

    Genrule added to generate model output file from spec

--- com/padsterprogramming/projection/spec/SpecGenerator.java
+++ com/padsterprogramming/projection/spec/SpecGenerator.java
@@ -8,6 +8,7 @@ import com.padsterprogramming.projection.tpl.TextTemplate;
 import com.padsterprogramming.projection.tpl.TextTemplateParser;
 import com.padsterprogramming.projection.tpl.TextTemplateWriter;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,15 +16,18 @@ import java.io.InputStream;
 public class SpecGenerator {
   // HACK - make genrule instead.
   public static void main(String[] args) throws IOException {
+    String specPath = args[0];
+    String[] outputPackageAndClass = parseFileName(args[1]);
+
+    StringMap<com.padsterprogramming.projection.model.Type>
+        specData = new SpecReader().parseStringMap(new FileInputStream(args[0]));
+
     String templatePath = "com/padsterprogramming/projection/spec/basetype.tpl.txt";
     TextTemplate.ContextNode template = TextTemplateParser.parse(resourceToStream(templatePath));
 
-    String specPath = "com/padsterprogramming/projection/spec/model.spec";
-    StringMap<com.padsterprogramming.projection.model.Type>
-        specData = new SpecReader().parseStringMap(resourceToStream(specPath));
-
     // HACK - need to do this via bound template method instead.
-    StringMap<com.padsterprogramming.projection.model.Type> processed = processData(specData, specPath);
+    StringMap<com.padsterprogramming.projection.model.Type> processed =
+        processData(specData, specPath, outputPackageAndClass[0], outputPackageAndClass[1]);
     String result = TextTemplateWriter.write(template, processed);
     System.out.println(result);
   }
@@ -32,15 +36,33 @@ public class SpecGenerator {
     return SpecGenerator.class.getClassLoader().getResourceAsStream(path);
   }
 
+  /** @return [package, class name] parsed from the file location. */
+  private static String[] parseFileName(String fileName) {
+    // TODO - verify a valid file name.
+    String ending = ".java";
+    int splitPoint = fileName.lastIndexOf("/");
+    if (splitPoint == -1 || !fileName.endsWith(ending)) {
+      throw new IllegalArgumentException("Output path must be of the form path/to/File.java");
+    }
+
+    return new String[] {
+        fileName.substring(0, splitPoint).replace('/', '.'),
+        fileName.substring(splitPoint + 1, fileName.length() - ending.length())
+    };
+  }
+
   // HACK - automate via template, see above.
   private static StringMap<com.padsterprogramming.projection.model.Type> processData(
-      StringMap<com.padsterprogramming.projection.model.Type> data, String filePath) {
+      StringMap<com.padsterprogramming.projection.model.Type> data, String filePath, String package_, String class_) {
     StringMapImpl<com.padsterprogramming.projection.model.Type> result = new StringMapImpl<>();
     result.set("file", Primitives.of(filePath));
+    result.set("WrapperClass", Primitives.of(class_));
 
     ((StringMapImpl<com.padsterprogramming.projection.model.Type>) data).keys().forEach(name -> {
       if (name.equals("package")) {
-        result.set(name, (Primitive<String>) data.get("package"));
+        Primitive<String> packageValue = (Primitive<String>) data.get("package");
+        // TODO - verify the package in the file is the same as package_ from the output.
+        result.set(name, packageValue);
       } else {
         result.set("name", Primitives.of(name));
         result.set("UpperName", Primitives.of(upperFirst(name)));
---
+++ com/padsterprogramming/projection/spec/model.spec
@@ -0,0 +1,12 @@
+{
+  "package": "com.padsterprogramming.projection.specz",
+
+  "Type": {
+    "name": "string",
+    "field": [{
+      "name": "string",
+      "type": "string",
+      "repeated": "boolean"
+    }]
+  }
+}
